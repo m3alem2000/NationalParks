4fbf36c67555fee70ac6f3ed231e62988b41da9e
Debora Lipetz <debora.lipetz@gmail.com>
2017-03-17 15:45:43 -0400

    survey page selenium page

--- src/test/java/com/techelevator/npgeek/pageobject/SurveyResultsPage.java
+++ src/test/java/com/techelevator/npgeek/pageobject/SurveyResultsPage.java
@@ -1,5 +1,25 @@
 package com.techelevator.npgeek.pageobject;
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class SurveyResultsPage {
 
+	private WebDriver webDriver;
+
+	public SurveyResultsPage(WebDriver webDriver){
+		this.webDriver = webDriver;
+	}
+
+	public boolean verifyParkName(String parkName){
+		//need to find the name of the park with most votes//
+		WebElement outputParkName = webDriver.findElement(By.id("parkName"));
+		return outputParkName.getText().equals(parkName);
+	}
+	
+	
+	
 }
