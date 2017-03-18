0778cb20c6b6eac9ee07efa3e4f0e9d5542a9306
Brian Cerney <brianecerney@gmail.com>
2017-03-18 16:59:27 -0400

    Moved Integration tests to separate package, added unit test classes for models.

--- src/test/java/com/techelevator/npgeek/unittests/WeatherTest.java
+++ src/test/java/com/techelevator/npgeek/unittests/WeatherTest.java
@@ -2,10 +2,20 @@ package com.techelevator.npgeek.unittests;
 
 import org.junit.Test;
 
+import com.techelevator.npgeek.parks.model.Weather;
+
 public class WeatherTest {
 	
 	@Test
-	public void setup() {
+	public void weatherObjectInitializesCorrectly() {
+		Weather testWeather = new Weather();
+		testWeather.setParkCode("GNP");
+		testWeather.setFiveDayForecastValue(1);
+		testWeather.setLowF(lowF);
+		testWeather.setHighF(highF);
+		testWeather.setLowC(lowC);
+		testWeather.setHighC(highC);
+		testWeather.setForecast(forecast);
 		
 	}
 
